7f022026b09192ac975f6f4f68d9f54d2cd27ccc
Ngala Junior <ngala.junior07@gmail.com>
2025-09-04 08:05:54 +0300

    merge typeDefs into one file <typeDefs>

--- src/schema/typedefs/type-defs.ts
+++ src/schema/typedefs/type-defs.ts
@@ -1,3 +1,93 @@
-import { userTypeDefs } from "./users.types-defs";
 
-export const typeDefs = [userTypeDefs]
+export const typeDefs = `#graphql 
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    role: String!
+    tickets: [Ticket]
+  }
+
+  type Match {
+    id: ID!
+    homeTeam: String!
+    awayTeam: String!
+    stadiumId: String!
+    date: String!
+    time: String!
+    gameType: String!
+    gameStage: String!
+    tickets: [Ticket]
+    stadium: Stadium
+  }
+
+  input AddMatch {
+    homeTeam: String!
+    awayTeam: String!
+    stadium: String!
+    date: String!
+    time: String!
+    gameType: String!
+    gameStage: String!
+  }
+
+  type Section {
+    vvip: Int
+    vip: Int
+    normal: Int
+    mediaBox: Int
+  }
+
+  type Stadium {
+    id: ID!
+    name: String!
+    location: String!
+    capacity: Int!
+    section: Section
+    matches: [Match]
+  }
+
+  type Ticket {
+    id: ID!
+    section: String
+    scannerCode: String!
+    matchId: String!
+    userId: String!
+  }
+  
+  input CreateUser {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    confirmPassword: String!
+  }
+
+  input LoginUser {
+    email: String!
+    password: String!
+  }
+
+  type AuthPayload {
+    token: String!
+  }
+
+  type SignupRes {
+    success: String!
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User!
+    matches: [Match!]!
+    match(id: ID!): Match!
+  }
+
+  type Mutation {
+    createUser(input: CreateUser): SignupRes!
+    loginUser(input: LoginUser): AuthPayload!
+    addMatch(input: AddMatch): Match!
+  }
+`;
